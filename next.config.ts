ca37dd2f0731deca7f28800d0bac67a6953bb236
Rayyan Naveed <164235692+mrayyannaveed@users.noreply.github.com>
2024-10-29 16:53:01 +0500

    update

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  eslint = {
-    ignoreDuringBuilds: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+      ignoreDuringBuilds: true,
   },
-  }
+}
 
 export default nextConfig;
