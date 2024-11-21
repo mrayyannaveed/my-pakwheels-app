be26a3049595ad80541bc6735a832189b975ba66
Rayyan Naveed <164235692+mrayyannaveed@users.noreply.github.com>
2024-11-21 17:21:47 +0500

    Update form and thank u

--- src/app/Thank-you/page.tsx
+++ src/app/Thank-you/page.tsx
@@ -1,7 +1,7 @@
 export default function ThankYou(){
     return(
         <div>
-            <div className="h-[88vh] flex flex-col items-center justify-center">
+            <div className="h-[78vh] sm:h-[80vh] md:h-[82vh] lg:h-[84vh] xl:h-[86vh] flex flex-col items-center justify-center">
                 <h1 className="text-xl text-center sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl text-blue-500 font-bold p-20"><i>Thank you for shopping!!!</i></h1>
                 <p className="text-lg text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl ">Your order will be delivered in 3 Business days</p>
             </div>
