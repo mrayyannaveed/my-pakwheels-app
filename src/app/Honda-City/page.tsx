9575412358545fcce6689a90349d8285e8378f4f
Rayyan Naveed <164235692+mrayyannaveed@users.noreply.github.com>
2024-11-21 14:56:14 +0500

    Change img name

--- src/app/Honda-City/page.tsx
+++ src/app/Honda-City/page.tsx
@@ -6,7 +6,7 @@ export default function HondaCity(){
             <div className="w-[65%] m-auto">
             <h1 className="underline text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl text-center font-bold">Honda City 2024 Price in Pakistan, Images, Reviews & Specs</h1>
             <div className="flex flex-col items-center">
-            <img className="py-10" src="/Honda_City.jpg" height={350} width={350} alt="City" />
+            <img className="py-10" src="/City.jpg" height={350} width={350} alt="City" />
             <p className="text-center text-xs sm:text-sm md:text-base lg:text-xl xl:text-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe labore necessitatibus voluptates rerum odio, placeat quasi unde, distinctio modi ipsam eaque eos sint ab rem quia sed quas excepturi. Dolore aliquam enim omnis atque veritatis, maiores adipisci dolores voluptas dolorem quae voluptatibus, laborum, doloribus quasi explicabo obcaecati beatae. Illo rem culpa dolor, ut mollitia eveniet maxime quam dignissimos reprehenderit. Eum explicabo error expedita obcaecati itaque libero dignissimos harum, inventore tempora iure nostrum qui? Nihil ex earum nemo non omnis! Eum, magni illo, corporis minus ratione id natus molestias ea inventore vitae distinctio doloremque quasi quae fugit commodi velit nemo doloribus.</p>
             </div>
             <h1 className="text-green-400 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl text-center my-12 font-bold">PKR 58,00,000</h1>
